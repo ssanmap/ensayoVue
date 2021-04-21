cd6e16d72ce9cc190db490829543bf66a480722a
Sebastian <sebastian.smp21@gmail.com>
2021-04-20 23:49:15 -0400

    agregando vuex

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -1,14 +1,22 @@
 <template>
     <div>
-        <h1>hello soy el inicio</h1>
+        <h1>hello soy el inicio {{nombre}}</h1>
     </div>
 </template>
 
 <script>
+
+import {mapState} from 'vuex'
     export default {
         
+        computed: mapState({
+            nombre: function (state){
+                return state.nombre
+            }
+        }),
     }
 </script>
+
 
 <style scoped>
 
